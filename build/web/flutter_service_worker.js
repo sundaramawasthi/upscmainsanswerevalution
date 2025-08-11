8783dfea9174fe8f5828d2113913d4bb236888b5
sundaramawasthi <sawasthi0001@gmail.com>
2025-08-11 18:43:11 +0530

    Add

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "75e4f7a8ade35e8110bff4ed2b2f50e2
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "1ed0b4b7926ab3babad7ca793c7f0166",
+"flutter_bootstrap.js": "9c2b58d322570cc526c5039afc51ca76",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
